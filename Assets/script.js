87c874ad644b4ffb1e4b016daba049330c620858
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-02-25 22:03:26 -0500

    created vari to store api key and got elements by id

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+const apikey = '32bccd202684ad1da1619bdacd8aaf32';
+const form = document.getElementById('city-form');
+const cityInput = document.getElementById('city-input');
+const citylist = document.getElementById('city-list');
+const current = document.getElementById('current');
+const fiveDay = document.getElementById('five-day');
