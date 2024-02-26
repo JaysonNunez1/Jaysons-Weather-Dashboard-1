5f1fc3c2d4e180bba53920a6a63028ff5e168a92
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-02-25 22:24:20 -0500

    created a function to update the current weather section

--- Assets/script.js
+++ Assets/script.js
@@ -9,5 +9,19 @@ async function fetchWeather(city) {
     const geoUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial';
     const geoResponse = await fetch(geoUrl);
     const geoData = await geoResponse.json();
+    
+    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=${geoData.coord.lat}&lon=${geoData.coord.lon}&appid=${apiKey}&units=imperial';
+    const forecastResponse = await fetch(forecastUrl);
+    const forecastData = await forecastResponse.json(); 
     return {geoData,forecastData};
+}
+
+function updateCurrentWeather(data){
+    const {geoData, forecastData} = data;
+    current.style.display = 'block';
+    document.getElementById('city').textContent = geoData.name;
+    document.getElementById('date').textContent = moment().format('MMMM Do YYYY');
+    document.getElementById('temp').textContent = `Temperature: ${forecastData.list[0].main.temp}°F`;
+    document.getElementById('humidity').textContent = `Humidity: ${forecastData.list[0].main.humidity}%`;
+    document.getElementById('wind').textContent = `Wind Speed: ${forecastData.list[0].wind.speed} MPH`;
 }
