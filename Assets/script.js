d0f41564529017f52a8518e83dd66241edf083b5
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-02-25 22:08:28 -0500

    created function to fetch the weather data from the api

--- Assets/script.js
+++ Assets/script.js
@@ -4,3 +4,10 @@ const cityInput = document.getElementById('city-input');
 const citylist = document.getElementById('city-list');
 const current = document.getElementById('current');
 const fiveDay = document.getElementById('five-day');
+
+async function fetchWeather(city) {
+    const geoUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial';
+    const geoResponse = await fetch(geoUrl);
+    const geoData = await geoResponse.json();
+    return {geoData,forecastData};
+}
